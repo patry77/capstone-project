95bdb723c2a5e0d84feff987b88339547b419a7d
patryk <patry28011@gmail.com>
2023-11-07 13:27:31 +0100

    Update Ansible folder structure

---
+++ terraform/pipelineVM/outputs.tf
@@ -0,0 +1,3 @@
+output "vm_public_ip" {
+  value = module.compute.vm_public_ip
+}
---
+++ terraform/pipelineVM/variables.tf
@@ -0,0 +1,15 @@
+variable project_id {
+  description = "Project ID"
+  type        = string
+}
+
+variable region {
+  description = "Region"
+  type        = string
+}
+
+variable zone {
+  description = "Zone"
+  type        = string
+}
+
