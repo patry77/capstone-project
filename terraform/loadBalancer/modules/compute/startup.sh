843567cafbb97214dccd7d59a176b29ec2d25b4a
patryk <patry28011@gmail.com>
2023-11-20 14:44:14 +0100

    Debug Jenkins deployment

--- terraform/loadBalancer/modules/compute/startup.sh
+++ terraform/loadBalancer/modules/compute/startup.sh
@@ -4,5 +4,5 @@ docker_registry_ip=$TF_VAR_insecure_host
 
 
 echo '{
-  "insecure-registries": ["'"${docker_registry_ip}"':8082"]
+  "insecure-registries": ["$docker_registry_ip}:8082"]
 }' | sudo tee /etc/docker/daemon.json
