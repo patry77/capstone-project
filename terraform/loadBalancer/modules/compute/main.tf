a4437fdc6bce31fba81c46208eb9b92856a493ac
patryk <patry28011@gmail.com>
2023-11-20 14:55:13 +0100

    Update loadbalancer script path

--- terraform/loadBalancer/modules/compute/main.tf
+++ terraform/loadBalancer/modules/compute/main.tf
@@ -31,7 +31,7 @@ resource "google_compute_instance_template" "vm_instance" {
   labels = {
     container-vm = module.gce-container.vm_container_label
   }
-  metadata_startup_script = file("./startup.sh")
+  metadata_startup_script = file("./modules/compute/startup.sh")
 }
 
 resource "google_compute_instance_group_manager" "instance_group_manager" {
