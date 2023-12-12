9ba112ec9eb3700c3d0df321fb4cc14a658cc91e
patry77 <patry28011@gmail.com>
2023-12-12 13:18:28 +0100

    Delete required version

--- terraform/loadBalancer/provider.tf
+++ terraform/loadBalancer/provider.tf
@@ -9,7 +9,7 @@ terraform {
       version = "~> 4.0"
     }
   }
-  required_version = "~> 1.5.0"
+  # required_version = "~> 1.5.0"
 
 }
 
--- terraform/pipelineVM/provider.tf
+++ terraform/pipelineVM/provider.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 4.51"
     }
   }
-    required_version = "~> 1.5.0"
+    # required_version = "~> 1.5.0"
 }
 
 provider "google" {
